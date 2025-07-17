2964eaba47afd89a608a5ec88cd0b54aaf75f307
ZEE <6611130043@mut.ac.th>
2025-07-17 16:28:37 +0700

    commit3

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-    echo "This subject is Git Tutorial!!";
-    echo "This subject is Git Tutorial!!";
-
-    echo "Add file เพิ่มเติมแล้ว";
-    echo "แก้บัคเรียบร้อย";
+    echo "this subject is";
     
 ?>
