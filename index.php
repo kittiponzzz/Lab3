c106ce2911bd5d9d29a247b1f66f379cd7cdf88d
kittiponzzz <kittiponzee@gmail.com>
2025-07-17 16:20:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     echo "This subject is Git Tutorial!!";
     echo "This subject is Git Tutorial!!";
 
-    echo "Add file เพิ่มเติมแล้ว"
-    echo "แก้บัคเรียบร้อย"
+    echo "Add file เพิ่มเติมแล้ว";
+    echo "แก้บัคเรียบร้อย";
     
 ?>
